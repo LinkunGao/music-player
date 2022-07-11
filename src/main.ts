adc3b353121becec553373375c59d03c38522b21
LinkunGao <gaolinkun123@gmail.com>
2022-07-12 11:58:29 +1200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Vue from "vue";
+
 import VueGtag from "vue-gtag";
 
 const a = createApp(App);
 a.use(router).mount("#app");
-
 a.use(VueGtag, {
   config: { id: "G-KB7T3J4T20" },
 });
